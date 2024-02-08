3c04eb35155f6d86c5aeff8bf39abdbd4a682f30
Antal <antal@pricempire.com>
2024-02-08 18:46:07 +0100

    Refactor formatResponse method to handle Stickers, Agents, and Graffiti

--- src/modules/inspect/format.service.ts
+++ src/modules/inspect/format.service.ts
@@ -83,6 +83,125 @@ export class FormatService implements OnModuleInit {
 
     public formatResponse(asset: Asset) {
         const weapon = this.schema.weapons[asset.defIndex]
+
+        if (!weapon) {
+            if (asset.defIndex === 1209) {
+                // Sticker
+
+                return {
+                    iteminfo: {
+                        origin: asset.origin,
+                        quality: asset.quality,
+                        rarity: asset.rarity,
+                        a: asset.assetId,
+                        d: asset.d,
+                        paintseed: asset.paintSeed,
+                        defindex: asset.defIndex,
+                        paintindex: asset.paintIndex,
+                        stickers: [],
+                        itemid: asset.assetId,
+                        floatid: asset.assetId,
+                        floatvalue: asset.paintWear,
+                        s: asset.ms.startsWith('7656') ? asset.ms : '0',
+                        m: asset.ms.startsWith('7656') ? '0' : asset.ms,
+                        imageurl: '',
+                        min: 0,
+                        max: 0,
+                        weapon_type: 'Sticker',
+                        item_name: 'Sticker',
+                        rarity_name: this.rarities[asset.rarity],
+                        quality_name: this.qualities[asset.quality],
+                        origin_name: this.origins[asset.origin],
+                        wear_name: '',
+                        full_item_name:
+                            this.schema.stickers[asset.stickers[0].sticker_id]
+                                .market_hash_name,
+                    },
+                }
+            } else if (this.schema.agents[asset.defIndex]) {
+                // Agents
+
+                const agent = this.schema.agents[asset.defIndex]
+
+                return {
+                    iteminfo: {
+                        origin: asset.origin,
+                        quality: asset.quality,
+                        rarity: asset.rarity,
+                        a: asset.assetId,
+                        d: asset.d,
+                        paintseed: asset.paintSeed,
+                        defindex: asset.defIndex,
+                        paintindex: asset.paintIndex,
+                        stickers: asset.stickers.map((sticker) => ({
+                            stickerId: sticker.sticker_id,
+                            slot: sticker.slot,
+                            rotation: sticker.rotation,
+                            wear: sticker.wear,
+                            offsetX: sticker.offset_x,
+                            offsetY: sticker.offset_y,
+                            scale: sticker.scale,
+                            name: this.schema.stickers[
+                                sticker.sticker_id
+                            ].market_hash_name.replace('Patch | ', ''),
+                        })),
+                        itemid: asset.assetId,
+                        floatid: asset.assetId,
+                        floatvalue: asset.paintWear,
+                        s: asset.ms.startsWith('7656') ? asset.ms : '0',
+                        m: asset.ms.startsWith('7656') ? '0' : asset.ms,
+                        imageurl: agent.image,
+                        min: 0,
+                        max: 0,
+                        weapon_type: 'Agent',
+                        item_name: agent.market_hash_name.split(' | ')[0],
+                        rarity_name: this.rarities[asset.rarity],
+                        quality_name: this.qualities[asset.quality],
+                        origin_name: this.origins[asset.origin],
+                        wear_name: '',
+                        full_item_name: agent.market_hash_name,
+                    },
+                }
+            } else if (asset.defIndex === 1349) {
+                return {
+                    iteminfo: {
+                        origin: asset.origin,
+                        quality: asset.quality,
+                        rarity: asset.rarity,
+                        a: asset.assetId,
+                        d: asset.d,
+                        paintseed: asset.paintSeed,
+                        defindex: asset.defIndex,
+                        paintindex: asset.paintIndex,
+                        stickers: asset.stickers.map((sticker) => ({
+                            stickerId: sticker.sticker_id,
+                            slot: sticker.slot,
+                            rotation: sticker.rotation,
+                            wear: sticker.wear,
+                            offsetX: sticker.offset_x,
+                            offsetY: sticker.offset_y,
+                            scale: sticker.scale,
+                            name: sticker.sticker_id,
+                        })),
+                        itemid: asset.assetId,
+                        floatid: asset.assetId,
+                        floatvalue: asset.paintWear,
+                        s: asset.ms.startsWith('7656') ? asset.ms : '0',
+                        m: asset.ms.startsWith('7656') ? '0' : asset.ms,
+                        imageurl: '',
+                        min: 0,
+                        max: 0,
+                        weapon_type: 'Graffiti',
+                        item_name: 'Graffiti',
+                        rarity_name: this.rarities[asset.rarity],
+                        quality_name: this.qualities[asset.quality],
+                        origin_name: this.origins[asset.origin],
+                        wear_name: '',
+                        full_item_name: 'Graffiti',
+                    },
+                }
+            }
+        }
         const paint = weapon.paints[asset.paintIndex]
         const wear = this.getWear(asset.paintWear)
 
@@ -111,8 +230,9 @@ export class FormatService implements OnModuleInit {
                     offsetX: sticker.offset_x,
                     offsetY: sticker.offset_y,
                     scale: sticker.scale,
-                    name: this.schema.stickers[sticker.sticker_id]
-                        .market_hash_name,
+                    name: this.schema.stickers[
+                        sticker.sticker_id
+                    ].market_hash_name.replace('Sticker | ', ''),
                 })),
                 itemid: asset.assetId,
                 floatid: asset.assetId,
