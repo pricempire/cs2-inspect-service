1f2692f9926dab898aadda910ae35617ae53fe95
Antal <antal@pricempire.com>
2024-02-08 18:59:56 +0100

    Refactor FormatService class to remove commented out code

--- src/modules/inspect/format.service.ts
+++ src/modules/inspect/format.service.ts
@@ -259,43 +259,6 @@ export class FormatService implements OnModuleInit {
                 full_item_name,
             },
         }
-
-        /*
-        {
-            "iteminfo": {
-                "stickers": [
-                    {
-                        "slot": 0,
-                        "stickerId": 5935,
-                        "codename": "csgo10_blue_gem_glitter",
-                        "material": "csgo10/blue_gem_glitter",
-                        "name": "Blue Gem (Glitter)"
-                    }
-                ],
-                "itemid": "35675800220",
-                "defindex": 1209,
-                "paintindex": 0,
-                "rarity": 4,
-                "quality": 4,
-                "paintseed": 0,
-                "inventory": 261,
-                "origin": 8,
-                "s": "76561198023809011",
-                "a": "35675800220",
-                "d": "12026419764860007457",
-                "m": "0",
-                "floatvalue": 0,
-                "min": 0.06,
-                "max": 0.8,
-                "weapon_type": "Sticker",
-                "item_name": "-",
-                "rarity_name": "Remarkable",
-                "quality_name": "Unique",
-                "origin_name": "Found in Crate",
-                "full_item_name": "Sticker | Blue Gem (Glitter)"
-            }
-        }
-        */
     }
 
     private getWear(wear: number) {
