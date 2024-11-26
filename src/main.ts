a8f4ffee4b7e990dd978e124cf10759712bffda3
Antal <antal.koti@pm.me>
2024-11-26 02:19:41 +0100

    Refactor main.ts to add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { MainModule } from './modules/main.module'
 import 'dotenv/config'
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(MainModule)
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     await app.listen(3000)
 }
 bootstrap()
