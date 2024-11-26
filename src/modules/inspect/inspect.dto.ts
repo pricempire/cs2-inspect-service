16b65cf9c435f6c91f4fdc96ddd134f565830a22
Antal <antal.koti@pm.me>
2024-11-26 02:30:26 +0100

    Refactor InspectDto to improve Steam URL validation

--- src/modules/inspect/inspect.dto.ts
+++ src/modules/inspect/inspect.dto.ts
@@ -40,7 +40,7 @@ export class InspectDto {
     @IsOptional()
     @IsString()
     // example: steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198023809011A40368145941D14586214085613790969
-    @Matches(/^steam:\/\/rungame\/730\/\d+\/[\w\d]+\/[\w\d]+$/, {
+    @Matches(/^steam:\/\/rungame\/730\/.*?\/.*?csgo_econ_action_preview.*?[SM]7656\d{13}A\d+D\d+$/, {
         message: 'Invalid Steam URL',
     })
     url?: string
