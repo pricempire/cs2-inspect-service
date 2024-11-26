a7f5d57fa81160923327f3fc839da9147628ff25
Antal <antal.koti@pm.me>
2024-11-26 06:48:08 +0100

    Refactor main.ts to handle uncaught exceptions and unhandled rejections

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,14 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     await app.listen(3000, '0.0.0.0')
 }
+
+process.on('uncaughtException', err => {
+    // console.log(`Uncaught Exception: ${err.message}`)
+    // process.exit(1)
+})
+process.on('unhandledRejection', (reason, promise) => {
+    // console.log('Unhandled rejection at ', promise, `reason: ${reason.message}`)
+    // process.exit(1)
+})
+
 bootstrap()
